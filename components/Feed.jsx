f753ed47aa18cc2a2821e4aa8d6ff5b9b32330e6
ffjaervik <fabianfjaervik@gmail.com>
2023-06-05 17:18:46 +0100

    debug

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,11 +4,13 @@ import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
 function PromptCardList({ data, handleTagClick }) {
-  return <div className="mt-16 prompt_layout">
-    {data.map((post) => (
-      <PromptCard key={post.id} post={post} handleTagClick={handleTagClick} />
-    ))}
-  </div>;
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard key={post.id} post={post} handleTagClick={handleTagClick} />
+      ))}
+    </div>
+  );
 }
 
 export default function Feed() {
