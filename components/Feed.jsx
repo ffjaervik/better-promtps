c9156c1688cba2a5f887525b32db258d21e60ee8
ffjaervik <fabianfjaervik@gmail.com>
2023-06-05 16:47:52 +0100

    get all prompts api route

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
+
+function PromptCardList({ data, handleTagClick }) {
+  return <div className="mt-16 prompt_layout">
+    {data.map((post) => (
+      <PromptCard key={post.id} post={post} handleTagClick={handleTagClick} />
+    ))}
+  </div>;
+}
 
 export default function Feed() {
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  function handleSearchChange(e) {}
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
-    <div>Feed</div>
-  )
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
 }
