f70214d0bd0d2bd9520c4ea868d3670616024243
ffjaervik <fabianfjaervik@gmail.com>
2023-06-05 14:58:36 +0100

    form component

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import Link from "next/link";
 
-export default function Form() {
+export default function Form({
+  type,
+  post,
+  setPost,
+  submitting,
+  handleSubmit,
+}) {
   return (
-    <div>Form</div>
-  )
+    <section className="flex-col w-full max-w-full flex-start">
+      <h1 className="text-left head_text">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="max-w-md text-left desc ">
+        {type} and share amazing prompts with the world, and let your
+        imagination run wild with any AI-powered platform.
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col w-full max-w-2xl mt-10 gap-7 glassmorphism"
+      >
+        <label>
+          <span className="text-base font-semibold text-gray-700 font-satoshi">
+            Your AI prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Write your prompt here..."
+            required
+            className="form_textarea"
+          />
+        </label>
+        <label>
+          <span className="text-base font-semibold text-gray-700 font-satoshi">
+            Tag{' '}
+            <span className="font-normal">(#product, #webdevelopment, #idea)</span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag:e.target.value })}
+            placeholder="#tag"
+            required
+            className="form_input"
+          />
+        </label>
+      </form>
+    </section>
+  );
 }
